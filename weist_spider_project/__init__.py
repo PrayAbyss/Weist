b718736fa0b35f1426e089175985c1e29757a5c7
Weist <95112449+PrayAbyss@users.noreply.github.com>
2026-03-12 17:35:48 +0800

    无

--- weist_spider_project/__init__.py
+++ weist_spider_project/__init__.py
@@ -4,10 +4,3 @@
 # @File:        __init__.py
 # @Software:    PyCharm
 # @Description:
-
-import os
-import sys
-
-BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-PROJECT_PATH = os.path.abspath(os.path.join(BASE_PATH, '..'))
-sys.path.append(PROJECT_PATH) if PROJECT_PATH not in sys.path else None
